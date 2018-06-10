c2379b0949293bbfba2543f8383d6ad71212dff6
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-06-10 16:13:29 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -20,6 +20,9 @@ def remove_strawberry(contacts)
   end
   contacts.each do |person, contact_details_hash|
   contact_details_hash.each do |attribute, data|
-    binding.pry
+    if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
   end
 end
